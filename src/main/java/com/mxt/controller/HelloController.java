1442047b8c83f72971bcc5accdce2713c3bc3cad
mxt <123456>
2018-02-07 12:00:55 +0800

    Application

--- src/main/java/com/mxt/controller/HelloController.java
+++ src/main/java/com/mxt/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.mxt.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +11,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "controller";
+        return "com/mxt/controller";
     }
 }
