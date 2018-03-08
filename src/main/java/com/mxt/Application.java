9ec9e3c851ddeddaf17bf08f57f07c5afc3fdc4a
mxt <123456>
2018-03-08 14:03:10 +0800

    集成springcloud

--- src/main/java/com/mxt/Application.java
+++ src/main/java/com/mxt/Application.java
@@ -3,10 +3,12 @@ package com.mxt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by mxt on 18-2-5.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan({"com.mxt.dao"})/** 扫描Mapper文件，使用此注解不需要在Mapper文件中加入@Mapper注解  **/
 public class Application {
